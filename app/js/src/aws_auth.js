6cf987d5f3bd4a011dac2c0307a9b74f2b3ef6c4
Tim Hemi (Ops) <tim@reinteractive.net>
2020-10-03 11:02:46 +1000

    album now has more room

--- app/js/src/aws_auth.js
+++ app/js/src/aws_auth.js
@@ -6,3 +6,41 @@ AWS.config.credentials = new AWS.CognitoIdentityCredentials({
 });
 
 
+// // Called when an identity provider has a token for a logged in user
+// function userLoggedIn(providerName, token) {
+//   AWS.config.credentials.params.Logins = creds.params.Logins || {};
+//   AWS.config.credentials.params.Logins[providerName] = token;
+
+//   // Expire credentials to refresh them on the next request
+//   AWS.config.credentials.expired = true;
+// }
+
+
+
+// var authenticationData = {
+//         Username : 'username',
+//         Password : 'password',
+//     };
+//     var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+//     var poolData = { UserPoolId : 'us-east-1_ExaMPle',
+//         ClientId : '1example23456789'
+//     };
+//     var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+//     var userData = {
+//         Username : 'username',
+//         Pool : userPool
+//     };
+//     var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+//     cognitoUser.authenticateUser(authenticationDetails, {
+//         onSuccess: function (result) {
+//             var accessToken = result.getAccessToken().getJwtToken();
+
+//             /* Use the idToken for Logins Map when Federating User Pools with identity pools or when passing through an Authorization Header to an API Gateway Authorizer */
+//             var idToken = result.idToken.jwtToken;
+//         },
+
+//         onFailure: function(err) {
+//             alert(err);
+//         },
+
+// });
